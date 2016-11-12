f22add236976b2ad1763bcc43520e38d1a792ae5
t010318 <t010318@ics857.icsuser.nagaokaut.ac.jp>
2016-11-12 17:35:34 +0900

    初期設定済み

--- WEB-INF/src/db/DataBaseProperty.java
+++ WEB-INF/src/db/DataBaseProperty.java
@@ -3,9 +3,9 @@ package db;
 public class DataBaseProperty {
 	static final public String DB_URL = "jdbc:sqlserver://mis.nagaokaut.ac.jp";
 
-	static final public String DB_NAME = "fyoshida";
+	static final public String DB_NAME = "123456";
 
-	static final public String DB_USERNAME = "fyoshida";
+	static final public String DB_USERNAME = "123456";
 
-	static final public String DB_PASSWORD = "fyoshida";
+	static final public String DB_PASSWORD = "12345678";
 }
